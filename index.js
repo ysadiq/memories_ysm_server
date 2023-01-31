2eab569e8c3965b0408dc94ae415ef23b85701a7
Yahya Saddiq <yahya.saddiq@gmail.com>
2023-01-31 17:05:53 +0300

    change port to 3001

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 // TODO: Secure your credentials
 // Create enviromental variable to store the connection url
 const CONNECTION_URL = 'mongodb+srv://ysaddiq:IwbiJ89IiE@cluster0.j5sky1w.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // useNewUrlParser + useUnifiedTopology used to avoid errors and warnings.
 mongoose.set('strictQuery', false);
