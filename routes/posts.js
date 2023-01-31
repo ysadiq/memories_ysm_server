e704ba8541f2dc8ad69bd6d8a4edebab6e7e5618
Yahya Saddiq <yahya.saddiq@gmail.com>
2023-01-31 17:22:01 +0300

    Add updatePost route

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,13 @@
 import express from 'express';
 
-import { getPosts, createPost } from '../controllers/posts.js';
+import { getPosts, createPost, updatePost } from '../controllers/posts.js';
 
 const router = express.Router();
 
 router.get('/', getPosts)
 router.post('/', createPost)
+/* PATCH is used here instead of put to allow the client to transmit
+ partial data that will be updated without changing the whole data. */
+router.patch('/:id', updatePost)
 
 export default router;
