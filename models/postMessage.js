ade498c466b4b6c8a5b6cc5025b076b616c67680
Yahya Saddiq <yahya.saddiq@gmail.com>
2023-01-31 17:06:32 +0300

    add timestamp post schema

--- models/postMessage.js
+++ models/postMessage.js
@@ -9,11 +9,9 @@ const postSchema = mongoose.Schema({
     likeCount: {
         type: Number,
         default: 0
-    },
-    createdAt:  {
-        type: Date,
-        default: new Date()
     }
+}, {
+    timestamps: true
 });
 
 // Convert schema into model
