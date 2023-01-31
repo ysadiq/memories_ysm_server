ad3becc553faf0c09b445cdf4853a990074cc973
Yahya Saddiq <yahya.saddiq@gmail.com>
2023-01-31 17:06:03 +0300

    fix CORS bug

--- index.js
+++ index.js
@@ -7,12 +7,11 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use('/posts', postRoutes);
 // TODO: Secure your credentials
 // Create enviromental variable to store the connection url
 const CONNECTION_URL = 'mongodb+srv://ysaddiq:IwbiJ89IiE@cluster0.j5sky1w.mongodb.net/?retryWrites=true&w=majority';
