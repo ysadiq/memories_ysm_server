da9c52e6c4c26d42597005c97049d80524b21aac
Yahya Saddiq <yahya.saddiq@gmail.com>
2023-02-01 09:50:13 +0300

    Add DeletePost route

--- controllers/posts.js
+++ controllers/posts.js
@@ -21,16 +21,28 @@ export const createPost = async (req, res) => {
 
         res.status(201).json(newPost);
     } catch (error) {
-        res.status(409).json({ message: error.message });
+        res.status(404).json({ message: error.message });
     }
 }
 
 export const updatePost = async (req, res) => {
-    const { id: _id } = req.params;
+    const { id } = req.params;
     const post = req.body;
-    if (mongoose.Types.ObjectId.isInvalid) 
-        return  res.status(404).send('No post with that id');
+    
+    if (!mongoose.Types.ObjectId.isValid(id)) return  res.status(404).send('No post with that id');
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
+   
     res.json(updatedPost);
+}
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
+
+    await PostMessage.findByIdAndRemove(id);
+
+    res.json({ message: 'Post deleted successfully!' });
+
 }
--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,14 @@
 import express from 'express';
 
-import { getPosts, createPost, updatePost } from '../controllers/posts.js';
+import { getPosts, createPost, updatePost, deletePost } from '../controllers/posts.js';
 
 const router = express.Router();
 
-router.get('/', getPosts)
-router.post('/', createPost)
+router.get('/', getPosts);
+router.post('/', createPost);
 /* PATCH is used here instead of put to allow the client to transmit
  partial data that will be updated without changing the whole data. */
-router.patch('/:id', updatePost)
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
 
 export default router;
