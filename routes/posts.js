0127306b14a24324124d648bf0748ed90a0493ce
Yahya Saddiq <yahya.saddiq@gmail.com>
2023-02-02 14:50:51 +0300

    add auth middleware

--- routes/posts.js
+++ routes/posts.js
@@ -1,15 +1,16 @@
 import express from 'express';
 
 import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
+import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.get('/', getPosts);
-router.post('/', createPost);
+router.post('/', auth, createPost);
 /* PATCH is used here instead of put to allow the client to transmit
  partial data that will be updated without changing the whole data. */
-router.patch('/:id', updatePost);
-router.delete('/:id', deletePost);
-router.patch('/:id/likePost', likePost);
+router.patch('/:id', auth, updatePost);
+router.delete('/:id', auth, deletePost);
+router.patch('/:id/likePost', auth, likePost);
 
 export default router;
