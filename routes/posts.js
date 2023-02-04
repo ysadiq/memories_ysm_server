e01b0314b6d6c4e456019e3d8ebeab994391e128
Yahya Saddiq <yahya.saddiq@gmail.com>
2023-02-04 12:18:18 +0300

    Add getPostsBySearch route

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
+import { getPostsBySearch, getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
 import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
+router.get('/search', getPostsBySearch);
 router.get('/', getPosts);
 router.post('/', auth, createPost);
 /* PATCH is used here instead of put to allow the client to transmit
