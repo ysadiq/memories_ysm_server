22f0d4178bde56b8d4cebc37ea798c72c4c25c4e
Yahya Saddiq <yahya.saddiq@gmail.com>
2023-02-05 10:42:21 +0300

    Add getPost route

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,14 @@
 import express from 'express';
 
-import { getPostsBySearch, getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
+import { getPostsBySearch, getPosts, getPost, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
 import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.get('/search', getPostsBySearch);
 router.get('/', getPosts);
+router.get('/:id', getPost);
+
 router.post('/', auth, createPost);
 /* PATCH is used here instead of put to allow the client to transmit
  partial data that will be updated without changing the whole data. */
